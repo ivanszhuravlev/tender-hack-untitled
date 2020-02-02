de52180fca35527fc85c2e5c79108cc65b633e2d
David Indzhiev <egniel@yandex.ru>
2020-02-02 23:28:42 +0300

    updates

--- backend/main.py
+++ backend/main.py
@@ -59,14 +59,50 @@ def autocomplete():
                 "term" : {
                     "field" : "Наменование"
                 }
-            }
+            },
+            "ngram" : {
+                "term" : {
+                    "string_distance": "ngram",
+                    "field" : "Наменование"
+                }
+            },
+            "levenshtein" : {
+                "term" : {
+                    "string_distance": "levenshtein",
+                    "field" : "Наменование"
+                }
+            },
+            "damerau_levenshtein" : {
+                "term" : {
+                    "string_distance": "damerau_levenshtein",
+                    "field" : "Наменование"
+                }
+            },
+            "jaro_winkler" : {
+                "term" : {
+                    "string_distance": "jaro_winkler",
+                    "field" : "Наменование"
+                }
+            },
         },
-        "size": 7
+        "size": 7,
     }
-    # print(body)
-    res = es.search(index="items", doc_type="products", body=body, params=params)
-    res = [r['text'] for r in res['suggest']['autocomplete'][0]['options']]
-    return jsonify(res)
+    r = es.search(index="items", doc_type="products", body=body, params=params)
+    print(r)
+    q = []
+    for k,v in body['suggest'].items():
+        if k == 'text':
+            continue
+        
+
+        res = r['suggest'][k]
+        if len(res):
+            r_opt = [r['text'] for r in res[0]['options']]
+        else:
+            r_opt = []
+        r_text = [z['text'] for z in res]
+        q.extend(r_opt + r_text)
+    return jsonify(list(set(q)))
 
 
 
