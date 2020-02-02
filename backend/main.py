0fd53767d1edd5b3054ca4ecc0db3cbbe19984b4
David Indzhiev <egniel@yandex.ru>
2020-02-02 03:23:09 +0300

    update

--- backend/main.py
+++ backend/main.py
@@ -1,58 +1,37 @@
 from datetime import datetime
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS, cross_origin
 from elasticsearch import Elasticsearch
-from utils import fill_data_from_csv
+from utils import create_index
 
 
-es = Elasticsearch(send_get_body_as='GET')
+es = Elasticsearch()
 
-app = Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-def index():
-    results = es.get(index='items', doc_type='products', id='1172027')
-    return jsonify(results['_source'])
+app = Flask(__name__, template_folder='dist')
+CORS(app)
 
 
-def create_index(es, index_name='items'):
-    created = False
-    # index settings
-    settings = {
-        "settings": {
-            "number_of_shards": 1,
-            "number_of_replicas": 0,
-            "analysis" : {
-                "analyzer" : {
-                    "whitespace_lowercase" : {
-                        "tokenizer" : "whitespace",
-                        "filter" : ["lowercase", "porter_stem"]
-                    }
-                }
-            }
-        },
-        "mappings": {
-        },
-    }
-    try:
-        if not es.indices.exists(index_name):
-            # Ignore 400 means to ignore "Index Already Exist" error.
-            es.indices.create(index=index_name, ignore=400, body=settings)
-            print('Created Index')
-        created = True
-        fill_data_from_csv(es, filename='test.csv')
-    except Exception as ex:
-        print(str(ex))
-    finally:
-        return created
+@app.route('/')
+def root():
+    return render_template('index.html')
 
 
 @app.route('/search', methods=['GET'])
 def search():
     q = request.args.get('q')
     res = es.search(index="items", doc_type="products", q=q)
-    return jsonify(res['hits']['hits'])
+    res = [r['_source'] for r in res['hits']['hits']]
+    return jsonify(res)
+
+
+@app.route('/search2', methods=['GET'])
+def search2():
+    q = request.args.get('q')
+    params = dict(request.args)
+    params.pop('q')
+    res = es.search(index="items", doc_type="products", q=q, params=params)
+    res = [r for r in res['hits']['hits']]
+    return jsonify(res)
 
 
 create_index(es)
-app.run(port=5000, debug=True)
--- backend/utils.py
+++ backend/utils.py
@@ -12,3 +12,33 @@ def fill_data_from_csv(es, filename, index='items'):
                 'body': row
             }
             es.create(**data)
+
+
+def create_index(es, index_name='items'):
+    created = False
+    # index settings
+    settings = {
+        "settings": {
+            "number_of_shards": 1,
+            "number_of_replicas": 0,
+            "analysis" : {
+                "analyzer" : {
+                    "whitespace_lowercase" : {
+                        "tokenizer" : "whitespace",
+                        "filter" : ["lowercase", "porter_stem"]
+                    }
+                }
+            }
+        },
+    }
+    try:
+        if not es.indices.exists(index_name):
+            # Ignore 400 means to ignore "Index Already Exist" error.
+            es.indices.create(index=index_name, ignore=400, body=settings)
+            print('Created Index')
+        created = True
+        fill_data_from_csv(es, filename='full_data.csv')
+    except Exception as ex:
+        print(str(ex))
+    finally:
+        return created
